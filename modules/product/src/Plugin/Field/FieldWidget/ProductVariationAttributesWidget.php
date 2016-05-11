6529960f6612f9ab20cc4254bafd4f1b4e36e5c9
Bojan Zivanovic <bojanz@gmail.com>
2016-05-10 19:36:11 -0500

    ..

--- modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
+++ modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
@@ -181,10 +181,10 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
       // - Hide attributes that have no values.
       // - Require attributes that have a value on each variation.
       if (empty($element['attributes'][$field_name]['#options'])) {
-        $element['attributes'][$field_name]['#access'] = FALSE;
+        //$element['attributes'][$field_name]['#access'] = FALSE;
       }
       if (!isset($element['attributes'][$field_name]['#empty_value'])) {
-        //$element['attributes'][$field_name]['#required'] = TRUE;
+        $element['attributes'][$field_name]['#required'] = TRUE;
       }
     }
 
